8a9fd8a8d8828ebdc7bd510274e5a67c39994292
madebygeelou <125709909+madebygeelou@users.noreply.github.com>
2024-07-31 17:12:21 -0700

    finished backend

--- server/index.js
+++ server/index.js
@@ -2,6 +2,8 @@ import express from 'express';
 import mongoose from 'mongoose';
 import cors from 'cors';
 import dotenv from 'dotenv';
+import eventRoutes from './routes/events.js';
+import userRoutes from './routes/users.js';
 
 dotenv.config();
 
@@ -9,6 +11,8 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 const MONGODB_URL = process.env.MONGODB_URL;
 
+app.use('/events', eventRoutes);
+app.use('/users', userRoutes);
 app.use(cors());
 app.use(express.json());
 
